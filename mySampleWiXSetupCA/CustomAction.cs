44217f6b6a3da3cc222833c4fb53852d4ef5be6d
Benoit Patra <benoit.patra@gmail.com>
2014-07-18 17:43:41 +0200

    finishing the wix structure: the custom actions that set the HKLM key containing the active setup stubPath for install/uninstall are symetric for the sake of simplicity.

--- mySampleWiXSetupCA/CustomAction.cs
+++ mySampleWiXSetupCA/CustomAction.cs
@@ -12,9 +12,21 @@ namespace mySampleWiXSetupCA
     public class CustomActions
     {
         [CustomAction]
-        public static ActionResult CaActiveSetupUnregister(Session session)
+        public static ActionResult CaActiveSetup_RemoveOpenHKCU(Session session)
         {
-          throw new NotImplementedException();
+            //     <RegistryKey  Root='HKLM' Key='Software\Microsoft\Active Setup\Installed Components\[ProductName]' >
+            //  <RegistryValue Action='write' Name='StubPath' Type='string' Value='[CREATE_HKCU_OPEN_CMD]' />
+            //</RegistryKey>
+            throw new NotImplementedException();
+        }
+
+        [CustomAction]
+        public static ActionResult CaActiveSetup_SetOpenHKCU(Session session)
+        {
+            //     <RegistryKey  Root='HKLM' Key='Software\Microsoft\Active Setup\Installed Components\[ProductName]' >
+            //  <RegistryValue Action='write' Name='StubPath' Type='string' Value='[CREATE_HKCU_OPEN_CMD]' />
+            //</RegistryKey>
+            throw new NotImplementedException();
         }
 
     }
