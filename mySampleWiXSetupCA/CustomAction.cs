e22a387d03944fd09ec4f6f7318185190677199c
Benoit Patra <benoit.patra@gmail.com>
2014-07-23 16:34:26 +0200

    Create custom actions for setting the HKCU component of active set up see issue #7. Custom actions are wrapped (to avoid logging logic duplication)

--- mySampleWiXSetupCA/CustomAction.cs
+++ mySampleWiXSetupCA/CustomAction.cs
@@ -13,103 +13,129 @@ namespace mySampleWiXSetupCA
     public class CustomActions
     {
         /// <summary>
-        /// Change the active setup responsible for invoking the .exe to put IsInstalled to false and pass the command for removing in the StubPath.
+        /// Change the HKLM active setup responsible for invoking the .exe to put IsInstalled to false and pass the command for removing in the StubPath.
         /// </summary>
         /// <param name="session"><seealso cref="CaParameters"/></param>
         /// <returns>success status</returns>
         [CustomAction]
         public static ActionResult CaActiveSetup_RemoveHKLM(Session session)
         {
-            try
+            Action action = () =>
+                {
+                    var registryAbstractor = new RegistryAbstractor(session);
+
+                    //Set the Active Setup ot IsInstalled = 0 and change the StubPath
+                    var caParams = CaParameters.ExtractFromSession(session);
+                    using (RegistryKey createHklmKey = registryAbstractor.OpenOrCreateHklmKey(caParams.RegistrySubKey))
+                    {
+                        UpdateActiveSetupHklmKey(createHklmKey, caParams.Default, caParams.CreateComponentId,
+                                                 caParams.CreateCommand, caParams.RemoveCommand, caParams.Version, false);
+                    }
+                };
+
+            var runner = new CaRunner(session);
+            return runner.RunAction(action, "CaActiveSetup_RemoveHKLM");
+        }
+
+        /// <summary>
+        /// This trick is used to remove the ActiveSetup as an installed component without needing a reloging.
+        /// see issue #8
+        /// </summary>
+        /// <param name="session"></param>
+        /// <returns></returns>
+        [CustomAction]
+        public static ActionResult CaActiveSetup_RemoveHKCU(Session session)
+        {
+            Action action = () =>
             {
-                session.Log("Start CaActiveSetup_RemoveOpenHKCU...");
                 var registryAbstractor = new RegistryAbstractor(session);
 
                 //Set the Active Setup ot IsInstalled = 0 and change the StubPath
                 var caParams = CaParameters.ExtractFromSession(session);
-                using(RegistryKey createHklmKey = registryAbstractor.OpenOrCreateHklmKey(caParams.RegistrySubKey))
+                registryAbstractor.DeleteHkcuKey(caParams.RegistrySubKey);
+            };
+
+            var runner = new CaRunner(session);
+            return runner.RunAction(action, "CaActiveSetup_RemoveHKCU");
+        }
+
+
+
+        /// <summary>
+        /// This trick is used to register the ActiveSetup as an installed component without needing a reloging, this is mandatory since we do not want to force reboot.
+        /// see issue #8
+        /// </summary>
+        /// <param name="session"></param>
+        /// <returns></returns>
+        [CustomAction]
+        public static ActionResult CaActiveSetup_SetHKCU(Session session)
+        {
+            Action action = () =>
+            {
+                var registryAbstractor = new RegistryAbstractor(session);
+
+                //Set the Active Setup ot IsInstalled = 0 and change the StubPath
+                var caParams = CaParameters.ExtractFromSession(session);
+                using (RegistryKey hkcuKey = registryAbstractor.OpenOrCreateHkcuKey(caParams.RegistrySubKey))
                 {
-                    if (createHklmKey != null)
-                    {
-                        UpdateActiveSetupKey(createHklmKey, caParams.Default, caParams.CreateComponentId,caParams.CreateCommand, caParams.RemoveCommand,caParams.Version,false);
-                    }
+                    UpdateHkcuActiveSetupKey(hkcuKey, caParams.Version);
                 }
-            }
-            catch (SecurityException ex)
-            {
-                session.Log("CaActiveSetup_RemoveOpenHKCU SecurityException" + ex.Message);
-                return ActionResult.Failure;
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                session.Log("CaActiveSetup_RemoveOpenHKCU UnauthorizedAccessException" + ex.Message);
-                return ActionResult.Failure;
-            }
-            catch (Exception ex)
-            {
-                session.Log("CaActiveSetup_RemoveOpenHKCU Exception" + ex.Message);
-                return ActionResult.Failure;
-            }
-            session.Log("... end CaActiveSetup_RemoveOpenHKCU, successful!");
-            return ActionResult.Success;
+            };
+
+            var runner = new CaRunner(session);
+            return runner.RunAction(action, "CaActiveSetup_SetHKCU");
         }
 
 
         /// <summary>
-        /// Update/create the active setup responsible for invoking the .exe to put IsInstalled to true and pass the command for installing in the StubPath.
+        /// Update/create the HKLM active setup responsible for invoking the .exe to put IsInstalled to true and pass the command for installing in the StubPath.
         /// </summary>
         /// <param name="session"><seealso cref="CaParameters"/></param>
         /// <returns>success status</returns>
         [CustomAction]
         public static ActionResult CaActiveSetup_SetHKLM(Session session)
         {
-            try
+            Action action = () =>
             {
-                session.Log("Start CaActiveSetup_SetOpenHKCU...");
                 var registryAbstractor = new RegistryAbstractor(session);
                 var caParams = CaParameters.ExtractFromSession(session);
-             
-              
+
                 //install the active setup that will set the OPEN key.
                 using (RegistryKey hklmKey = registryAbstractor.OpenOrCreateHklmKey(caParams.RegistrySubKey))
                 {
-                    UpdateActiveSetupKey(hklmKey, caParams.Default, caParams.CreateComponentId, caParams.CreateCommand, caParams.RemoveCommand, caParams.Version, true);
+                    UpdateActiveSetupHklmKey(hklmKey, caParams.Default, caParams.CreateComponentId, caParams.CreateCommand, caParams.RemoveCommand, caParams.Version, true);
                 }
-            }
-            catch (SecurityException ex)
-            {
-                session.Log("CaActiveSetup_SetOpenHKCU SecurityException" + ex.Message);
-                return ActionResult.Failure;
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                session.Log("CaActiveSetup_SetOpenHKCU UnauthorizedAccessException" + ex.Message);
-                return ActionResult.Failure;
-            }
-            catch (Exception ex)
-            {
-                session.Log("CaActiveSetup_SetOpenHKCU Exception" + ex.Message);
-                return ActionResult.Failure;
-            }
-            session.Log("... end CaActiveSetup_SetOpenHKCU, successful!");
-            return ActionResult.Success;
+            };
+
+            var runner = new CaRunner(session);
+            return runner.RunAction(action, "CaActiveSetup_SetHKLM");
         }
 
-        private static void UpdateActiveSetupKey(RegistryKey activeSetupKey, string defaultForKey, string componentId, string commandInstall, string commandUninstall, string version, bool isInstalled)
+        private static void UpdateActiveSetupHklmKey(RegistryKey hklmKey, string defaultForKey, string componentId, string commandInstall, string commandUninstall, string version, bool isInstalled)
+        {
+            if (hklmKey == null)
+            {
+                throw new ArgumentNullException("hklmKey");
+            }
+
+            hklmKey.SetValue("",defaultForKey);
+            hklmKey.SetValue("ComponentID", componentId, RegistryValueKind.String);
+            hklmKey.SetValue("StubPath",isInstalled ? commandInstall : commandUninstall, RegistryValueKind.String);
+
+            //Found that . cannot be used for version
+            //http://www.sepago.de/e/helge/2010/04/22/active-setup-explained
+            hklmKey.SetValue("Version",version.Replace('.',','),RegistryValueKind.String); 
+            hklmKey.SetValue("IsInstalled", isInstalled ? 1 : 0 ,RegistryValueKind.DWord);
+        }
+
+        //Only the version is required... for hkcu installed component of active setup
+        private static void UpdateHkcuActiveSetupKey(RegistryKey activeSetupKey, string version)
         {
             if (activeSetupKey == null)
             {
                 throw new ArgumentNullException("activeSetupKey");
             }
-
-            activeSetupKey.SetValue("",defaultForKey);
-            activeSetupKey.SetValue("ComponentID", componentId, RegistryValueKind.String);
-            activeSetupKey.SetValue("StubPath",isInstalled ? commandInstall : commandUninstall, RegistryValueKind.String);
-
-            //Found that . cannot be used for version
-            //http://www.sepago.de/e/helge/2010/04/22/active-setup-explained
-            activeSetupKey.SetValue("Version",version.Replace('.',','),RegistryValueKind.String); 
-            activeSetupKey.SetValue("IsInstalled", isInstalled ? 1 : 0 ,RegistryValueKind.DWord);
+            activeSetupKey.SetValue("Version", version.Replace('.', ','), RegistryValueKind.String); 
         }
     }
 }
